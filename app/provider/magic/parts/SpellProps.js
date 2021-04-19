a214be577a867ba212dd2fa7b7cd6dfd670b1017
TestBPMNCamunda <82809921+TestBPMNCamunda@users.noreply.github.com>
2021-04-19 12:40:28 +0200

    Add files via upload

---
+++ app/provider/magic/parts/SpellProps.js
@@ -0,0 +1,33 @@
+import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
+
+import {
+  is
+} from 'bpmn-js/lib/util/ModelUtil';
+
+
+export default function(group, element) {
+
+  // Only return an entry, if the currently selected
+  // element is a start event.
+
+  if (is(element, 'bpmn:StartEvent')) {
+    group.entries.push(entryFactory.textField({
+      id : 'spell',
+      description : 'Apply a black magic spell',
+      label : 'Spell',
+      modelProperty : 'spell'
+    }));
+    group.entries.push({
+      html: '<input type="file" id="fileUploaded"><br><br>',
+      description : 'File Upload',
+      label : 'File Upload',
+      id : 'fileUploader',
+      modelProperty : 'fileUploader',
+      set: function(element, values){
+        console.log(values.fileUploader);
+        element.businessObject.$attrs.fileUploader = values.fileUploader;
+        return;
+      }
+    });
+  }
+}
